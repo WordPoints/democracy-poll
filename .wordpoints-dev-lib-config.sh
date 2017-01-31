78538fdb2dbf30b5696544ba96871466eb638be6
JDGrimes <jdg@codesymphony.co>
2017-01-31 14:55:59 -0500

    Install Democracy Poll plugin
    When running the PHPUnit tests on Travis CI.

---
+++ .wordpoints-dev-lib-config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Install Democracy Poll when running tests.
+install-democracy-poll() {
+
+	mkdir -p /tmp/wordpress/src/wp-content/plugins/democracy-poll
+	curl -s https://downloads.wordpress.org/plugin/democracy-poll.5.3.4.6.zip > /tmp/democracy-poll.zip
+	unzip /tmp/democracy-poll.zip -d /tmp/wordpress/src/wp-content/plugins/
+}
+
+# Sets up custom configuration.
+function wordpoints-dev-lib-config() {
+
+	alias setup-phpunit="\setup-phpunit; install-democracy-poll"
+}
+
+# EOF
